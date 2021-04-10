83bef49cc83fd73b08c07fe3d5f98c3063ca19eb
thanhvinh8192 <thanhvinh8192@gmail.com>
2021-04-10 15:23:11 +0900

    Activity 11

---
+++ src/Thread/Activity11.java
@@ -0,0 +1,19 @@
+package Thread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Activity11 {
+    public static void main(String[] args) {
+        List<Thread> threadList = new ArrayList<>();
+        for(int i=1; i<=50; i++){
+            Thread thread = new Thread(new ThreadActivity11(i));
+            thread.setName("Thread-" + i);
+            threadList.add(thread);
+        }
+        for (Thread th : threadList) {
+            th.start();
+        }
+        
+    }
+}
---
+++ src/Thread/ThreadActivity11.java
@@ -0,0 +1,13 @@
+package Thread;
+
+public class ThreadActivity11 extends Thread {
+    int STT;
+    public ThreadActivity11(int STT) {
+        this.STT = STT;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + ":  STT= " + STT);
+    }
+}
